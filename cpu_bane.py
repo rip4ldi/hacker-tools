19fe4523e9499275989f12d44f549afd5668f9c5
Hedge Fleming <98241220+hedgenull@users.noreply.github.com>
2022-06-05 09:49:57 -0400

    Update cpu_bane.py

--- cpu_bane.py
+++ cpu_bane.py
@@ -3,5 +3,6 @@ import threading
 # DO NOT RUN THIS UNLESS YOU WANT YOUR COMPUTER TO DIE!
 
 def cpu_bane():
-  t = threading.Thread(target=cpu_bane)
-  t.start()
+  while True:
+    t = threading.Thread(target=cpu_bane)
+    t.start()
